b1a3b52855da1712f91fcff2dbe80d78824552e7
Tim Rezelj <tim.rezelj@tomasdse.com>
2025-06-19 11:59:07 +0200

    Prepared program for production deployment. We can deploy this configuration on 5 servers.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -11,60 +12,60 @@ import (
 )
 
 type Configuration struct {
-	ElectionTimeoutTime int `json:"electionTimeoutTime"`
-	NumberOfServers     int `json:"numberOfServers"`
+	ElectionTimeoutTime int      `json:"electionTimeoutTime"`
+	NumberOfServers     int      `json:"numberOfServers"`
+	ServerAddresses     []string `json:"serverAddresses"`
 }
 
-func ProcessConfigFile(configFile string) Configuration {
+func ProcessConfigFile(configFile string) ([]byte, Configuration) {
 	file, err := os.Open(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	fileBytes, err := ioutil.ReadAll(file)
+	content, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	var configuration Configuration
-	err = json.Unmarshal(fileBytes, &configuration)
+	err = json.Unmarshal(content, &configuration)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return configuration
+	return content, configuration
 }
 
 func main() {
-	configFile := "configuration.json"
+	configFile := "prodConfiguration.json"
+	serverNum := 0
 	if len(os.Args) > 1 {
-		configFile = os.Args[1]
+		serverNum, _ = strconv.Atoi(os.Args[1])
 	}
-	configuration := ProcessConfigFile(configFile)
+	content, configuration := ProcessConfigFile(configFile)
 	if (configuration.NumberOfServers % 2) == 0 {
 		fmt.Println("You specified even number of servers in configuration file. For stable program execution " +
 			"use odd number of servers.")
 	}
 	processes := make([]*exec.Cmd, configuration.NumberOfServers)
-	for i := 0; i < configuration.NumberOfServers; i++ {
-		file, err := os.Create("errorOutput" + strconv.Itoa(i) + ".txt")
-		if err != nil {
-			fmt.Println("Error creating file:", err)
-			return
-		}
-		defer file.Close()
 
-		cmd := exec.Command("go", "run", "runServer.go", strconv.Itoa(i), configFile)
-		cmd.Stdout = file
-		cmd.Stderr = file
-		cmd.Start()
-
-		processes[i] = cmd
-		fmt.Println(strconv.Itoa(cmd.Process.Pid) + "  ---  " + "localhost:5000" + strconv.Itoa(i))
+	file, err := os.Create("errorOutput" + strconv.Itoa(serverNum) + ".txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
 	}
-	for _, process := range processes {
-		err := process.Wait()
-		if err != nil {
-			panic(err)
-		}
+	defer file.Close()
+
+	cmd := exec.Command("go", "run", "./runServer/runServer.go", strconv.Itoa(serverNum))
+	cmd.Stdin = bytes.NewReader(content)
+	cmd.Stdout = file
+	cmd.Stderr = file
+	cmd.Start()
+
+	processes[0] = cmd
+	fmt.Println(strconv.Itoa(cmd.Process.Pid) + "  ---  " + "localhost:5000" + strconv.Itoa(serverNum))
+	err = processes[0].Wait()
+	if err != nil {
+		panic(err)
 	}
 }
