100732663dc79eb54ef5bb924c54237fb22c7475
Tim Rezelj <tim.rezelj@tomasdse.com>
2025-06-20 17:06:07 +0200

    Update

--- server/server.go
+++ server/server.go
@@ -380,9 +380,15 @@ func (server *Server) initializeNextIndex() {
 	}
 }
 
+func (server *Server) testTime(start time.Time) {
+	fmt.Println(time.Now(), " AppendEntry took ", time.Since(start), "  ", server.nextIndex)
+}
+
 // Receive and respond to AppendEntry
 
 func (server *Server) AppendEntry(ctx context.Context, in *sgrpc.AppendEntryMessage) (*sgrpc.AppendEntryResponse, error) {
+	start := time.Now()
+	defer server.testTime(start)
 	if int(in.Term) < server.currentTerm {
 		return server.receivesHeartbeatOrAppendEntryWithStaleTerm(), nil
 	}
